3566ac2c6f15493d456c21d94d2eb00d2f67e393
Irvin Ćatić <icatic1@etf.unsa.ba>
2020-10-30 13:15:52 +0100

    Prepravka

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+
+    private Artikl [] artikli = new Artikl[50];
+    private int duzina=0;
+
+    public boolean dodajArtikl(Artikl a) {
+        if(duzina < 50){
+            artikli[duzina] = a;
+            duzina++;
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<duzina; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl a =artikli[i];
+                for(int j=i;j<duzina-1;j++){
+                    artikli[j]=artikli[j+1];
+
+                }
+                artikli[duzina-1]=null;
+                duzina--;
+                return a;
+            }
+        }
+        return null;
+
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupna=0;
+        for(int i=0; i<duzina;i++){
+            ukupna=ukupna + artikli[i].getCijena();
+        }
+        return ukupna;
+    }
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+
+    private Artikl[] artikli=new Artikl[1000];
+    private int duzina;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for(int i=0; i<duzina; i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl a = artikli[i];
+                for(int j=i;j<duzina-1;j++){
+                    artikli[j]=artikli[j+1];
+
+                }
+                artikli[duzina-1]=null;
+                duzina--;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public void dodajArtikl(Artikl a) {
+        artikli[duzina]= a;
+        duzina++;
+
+    }
 }
+
+
